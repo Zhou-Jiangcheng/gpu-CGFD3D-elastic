973c5a5432d2bab32a88bf86c3bde1ad567fab09
李华林 <12131168@mail.sustech.edu.cn>
2023-11-27 14:36:28 +0800

    modify fdlib math, invert tmp matrix use double type

--- lib/fdlib_math.cu
+++ lib/fdlib_math.cu
@@ -5,9 +5,8 @@
 __host__ __device__
 void fdlib_math_invert3x3(float m[][3])
 {
-  float inv[3][3];
-  float det;
-  int i, j;
+  double inv[3][3];
+  double det;
 
   inv[0][0] = m[1][1]*m[2][2] - m[2][1]*m[1][2];
   inv[0][1] = m[2][1]*m[0][2] - m[0][1]*m[2][2];
@@ -25,9 +24,13 @@ void fdlib_math_invert3x3(float m[][3])
 
   det = 1.0f / det;
 
-  for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++)
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
       m[i][j] = inv[i][j] * det;
+    }
+  }
 
   return;
 }
@@ -35,13 +38,17 @@ void fdlib_math_invert3x3(float m[][3])
 __host__ __device__
 void fdlib_math_matmul3x3(float A[][3], float B[][3], float C[][3])
 {
-  int i, j, k;
-  for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++){
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
       C[i][j] = 0.0;
-      for (k = 0; k < 3; k++)
+      for (int k = 0; k < 3; k++)
+      {
         C[i][j] += A[i][k] * B[k][j];
+      }
     }
+  }
 
   return;
 }
@@ -58,10 +65,11 @@ void fdlib_math_cross_product(float *A, float *B, float *C)
 __host__ __device__
 float fdlib_math_dot_product(float *A, float *B)
 {
-  int i;
   float result = 0.0;
-  for (i = 0; i < 3; i++)
+  for (int i = 0; i < 3; i++)
+  {
     result += A[i] * B[i];
+  }
 
   return result;
 }
@@ -89,14 +97,20 @@ float fdlib_math_dist_point2plane(float x0[3], float x1[3], float x2[3], float x
 __host__ __device__
 void fdlib_math_bubble_sort(float a[], int index[], int n)
 {
-  int i, j;float tmp;
+  float tmp;
 
-  for (i = 0; i < n; i++) index[i] = i;
+  for (int i = 0; i < n; i++) 
+  {
+    index[i] = i;
+  }
   int tmpi;
 
-  for (j = 0; j < n-1; j++)
-    for (i = 0; i < n-1-j; i++){
-      if(a[i] > a[i+1]){
+  for (int j = 0; j < n-1; j++)
+  {
+    for (int i = 0; i < n-1-j; i++)
+    {
+      if(a[i] > a[i+1])
+      {
         tmp = a[i];
         a[i] = a[i+1];
         a[i+1] = tmp;
@@ -105,19 +119,26 @@ void fdlib_math_bubble_sort(float a[], int index[], int n)
         index[i+1] = tmpi;
       }
     }
+  }
 }
 
 __host__ __device__
 void fdlib_math_bubble_sort_int(int a[], int index[], int n)
 {
-  int i, j, tmp;
-
-  for (i = 0; i < n; i++) index[i] = i;
+  int tmp;
   int tmpi;
 
-  for (j = 0; j < n-1; j++)
-    for (i = 0; i < n-1-j; i++){
-      if(a[i] > a[i+1]){
+  for (int i = 0; i < n; i++) 
+  {
+    index[i] = i;
+  }
+
+  for (int j = 0; j < n-1; j++)
+  {
+    for (int i = 0; i < n-1-j; i++)
+    {
+      if(a[i] > a[i+1])
+      {
         tmp = a[i];
         a[i] = a[i+1];
         a[i+1] = tmp;
@@ -126,6 +147,7 @@ void fdlib_math_bubble_sort_int(int a[], int index[], int n)
         index[i+1] = tmpi;
       }
     }
+  }
 }
 
 /*
@@ -151,20 +173,21 @@ int fdlib_math_isPoint2InQuad(float px, float py, const float *vertx, const floa
     vx[2] = vertx[3]; vy[2] = verty[3];
     vx[3] = vertx[2]; vy[3] = verty[2];
 
-    for (int i = 0; i < 4; i++) {
-        // vector: edge
-        float vecte_x = i==3?(vx[0]-vx[3]):(vx[i+1]-vx[i]);
-        float vecte_y = i==3?(vy[0]-vy[3]):(vy[i+1]-vy[i]);
-        // vector: vertex to point
-        float vectp_x = px - vx[i];
-        float vectp_y = py - vy[i];
+    for (int i = 0; i < 4; i++) 
+    {
+      // vector: edge
+      float vecte_x = i==3?(vx[0]-vx[3]):(vx[i+1]-vx[i]);
+      float vecte_y = i==3?(vy[0]-vy[3]):(vy[i+1]-vy[i]);
+      // vector: vertex to point
+      float vectp_x = px - vx[i];
+      float vectp_y = py - vy[i];
 
-        float sign = vecte_x*vectp_y - vecte_y*vectp_x;
-        sign /= (sqrt(vecte_x*vecte_x + vecte_y*vecte_y));
-        // normalization
-        
-        if (sign < -1e-6)
-            return 0;
+      float sign = vecte_x*vectp_y - vecte_y*vectp_x;
+      sign /= (sqrt(vecte_x*vecte_x + vecte_y*vecte_y));
+      // normalization
+      
+      if (sign < -1e-6)
+          return 0;
     }
 
     return 1;
